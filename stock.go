601bc33b1862914022427e5cb808e5840115fe68
lzpel <18492524+lzpel@users.noreply.github.com>
2020-06-21 23:17:23 +0900

    stocknames.csv

--- stock.go
+++ stock.go
@@ -3,6 +3,9 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
+	"golang.org/x/text/encoding/japanese"
+	"golang.org/x/text/transform"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -12,26 +15,28 @@ import (
 	"strings"
 	"time"
 )
-func SaveStockNames(csvpath string){
+
+func SaveStockNames(csvpath string) {
 	file, e := os.OpenFile(csvpath, os.O_WRONLY|os.O_CREATE, 0666)
-	if e!=nil{
+	if e != nil {
 		log.Fatal(e)
 	}
 	defer file.Close()
-	for i:=0;i<82;{
-		time.Sleep(time.Second*2)
-		url:=fmt.Sprintf("http://portal.morningstarjp.com/StockInfo/sec/list?page=%d",i)
-		if code,body:=Curl(url);code==200{
-			i++
-			ex:=regexp.MustCompile(`(?m)<td class="tac"><a href="/StockInfo/info/snap/(\d+)">(.+)</a></td>\s+<td class="tac">東証１部</td>`)
-			code :=ex.FindAllStringSubmatch(body,-1)
-			for _,match:=range code{
-				file.WriteString(fmt.Sprintf("%s,%s",match[1],match[2]))
+	for page := 0; page < 82; {
+		time.Sleep(time.Second * 5)
+		url := fmt.Sprintf("http://portal.morningstarjp.com/StockInfo/sec/list?page=%d", page)
+		if code, body := Curl(url,true); code == 200 {
+			ex := regexp.MustCompile(`(\d+)"([^/]+)/a/tdtdclass="tac"東証１部`)
+			matches := ex.FindAllStringSubmatch(body, -1)
+			for _, match := range matches {
+				fmt.Fprintf(file,"%s,%s\n", match[1], match[2])
 			}
+			fmt.Println(page,len(matches))
+			page++
 		}
 	}
 }
-func GetStocks(csvpath,include string,colname,colcode int) []Stock {
+func GetStocks(csvpath, include string, colname, colcode int) []Stock {
 	//ファイルを開く
 	f, e := os.Open(csvpath)
 	if e != nil {
@@ -40,22 +45,22 @@ func GetStocks(csvpath,include string,colname,colcode int) []Stock {
 	defer f.Close()
 	//ファイルを読み込む
 	rows, e := csv.NewReader(f).ReadAll()
-	if e!=nil{
+	if e != nil {
 		log.Fatal(e)
 	}
-	returnStocks:=[]Stock{}
+	returnStocks := []Stock{}
 	for _, row := range rows {
-		flag:=false
-		for _, m:=range row {
-			flag=flag||strings.Contains(m,include)
+		flag := false
+		for _, m := range row {
+			flag = flag || strings.Contains(m, include)
 		}
-		if flag{
-			stock:=Stock{}
-			if stock.Code,e=strconv.Atoi(row[colcode]);e==nil{
-				stock.Prices=GetPrices(stock.Code)
-				returnStocks=append(returnStocks,stock)
-			}else{
-				log.Println("WARNING",e)
+		if flag {
+			stock := Stock{}
+			if stock.Code, e = strconv.Atoi(row[colcode]); e == nil {
+				stock.Prices = GetPrices(stock.Code)
+				returnStocks = append(returnStocks, stock)
+			} else {
+				log.Println("WARNING", e)
 			}
 		}
 	}
@@ -64,8 +69,7 @@ func GetStocks(csvpath,include string,colname,colcode int) []Stock {
 
 func GetPrices(code int) (outs []Price) {
 	url := fmt.Sprintf("https://stocks.finance.yahoo.co.jp/stocks/history/?code=%4d.T", code)
-	if code, body := Curl(url); code == 200 {
-		body = regexp.MustCompile(`(?m)[\s,<>]`).ReplaceAllString(body, "")
+	if code, body := Curl(url,false); code == 200 {
 		matches := regexp.MustCompile(`trtd(\d+年\d+月\d+日)/tdtd(\d+)/tdtd(\d+)/tdtd(\d+)/tdtd(\d+)/tdtd(\d+)/tdtd(\d+)/td/tr`).FindAllStringSubmatch(body, -1)
 		outs = make([]Price, len(matches))
 		for i := 0; i < len(matches); i++ {
@@ -78,13 +82,23 @@ func GetPrices(code int) (outs []Price) {
 			p.Close, _ = strconv.Atoi(matches[i][5])
 		}
 	}
-	if outs==nil||len(outs)<5{
-		log.Println("WARNING",code,outs)
+	if outs == nil || len(outs) < 5 {
+		log.Println("WARNING", code, outs)
 	}
 	return outs
 }
-func Curl(url string) (int, string) {
-	w, _ := http.Get(url)
-	b, _ := ioutil.ReadAll(w.Body)
-	return w.StatusCode, string(b)
+func Curl(url string, shiftjis bool) (int, string) {
+	var r io.Reader
+	w, e := http.Get(url)
+	if e==nil{
+		defer w.Body.Close()
+	}
+	if shiftjis {
+		r = transform.NewReader(w.Body, japanese.ShiftJIS.NewDecoder())
+	} else {
+		r = w.Body
+	}
+	bytes, _ := ioutil.ReadAll(r)
+	body := regexp.MustCompile(`(?m)[\s,<>]`).ReplaceAllString(string(bytes), "")
+	return w.StatusCode, body
 }
